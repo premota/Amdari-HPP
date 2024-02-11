08ad1a0e23b89fcf27eceae51e85da1457c3d142
premota <premota101@gmail.com>
2024-02-11 18:55:30 +0100

    exception point

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+editable = - "-e ."
+def find_requirements(file_path: str)-> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if editable in requirements:
+            requirements.remove(editable)
+
+
+
+set(
+    name = "House price prediction",
+    version = "0.1",
+    author = "precious",
+    author_email = "",
+    packages = find_packages(),
+    install_require = find_requirements("requirements.txt")
+)
---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+log_dir_name = f"{datetime.now().strftime("%m,_%d_%Y_%H_%M_%S")}.log"
+logs_path = os.path.join(os.getcwd(), "logs", log_dir_name)
+os.makedirs(logs_path, exist_ok = True)
+
+log_file_path = os.path.join(logs_path, "log-file.log")
+
+logging.basicConfig(
+    filename= log_file_path,
+    format = "[%(asctime)s] %(lineno)d -%(levelname)s - %(message)s",
+    level = logging.INFO,
+)
